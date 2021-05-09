898f3602c8ad657a9c3ce7deffabcf885114f56a
daviddelapuente <you@example.com>
2021-05-09 03:26:24 -0400

    exp10

---
+++ implementations/experimentsForSdk2tree/fexp10wave20-10two/gettotalsize.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <ftw.h>
+
+static unsigned int total = 0;
+
+int sum(const char *fpath, const struct stat *sb, int typeflag) {
+    total += sb->st_size;
+    return 0;
+}
+
+int main() {
+    
+    int i=5000000;
+    while(i<=325000000){
+ 	total=0;
+	char dirpath[1024];
+    	strcpy(dirpath,"./results/sizefiles/");
+	char intbuf[100];
+	sprintf(intbuf, "%i", i);
+	strcat(dirpath,intbuf);
+    	
+	if (ftw(dirpath, &sum, 1)) {
+        	perror("ftw");
+        	return 2;
+    	}
+    	printf("%u,\n", total);	
+	i+=5000000;
+    	}
+
+    return 0;
+}
